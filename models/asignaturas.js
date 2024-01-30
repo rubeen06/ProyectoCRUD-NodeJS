6fa4bbec501a9b63d557b8e4581b702f092a202d
Terry-Reynolds <terryreynoso@hotmail.com>
2024-01-30 19:23:05 +0100

    Actualizado view y routes de asignaturas

---
+++ models/asignaturas.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AsignaturasSchema = Schema({
+  nombre: {
+    type: String,
+    required: true
+  },
+  planEstudios: {
+    type: String,
+    required: true
+  },
+  cuatrimestre: {
+    type: Number,
+    required: true
+  },
+  curso: {
+    type: Number,
+    required: true
+  },
+  software: [{
+    type: String
+  }]
+});
+
+
+AsignaturasSchema.methods.findAll= async function (usuario) {
+  const Asignatura = mongoose.model("asignaturas", AsignaturasSchema);
+  return await Asignatura.find({'usuario':usuario});
+};
+
+AsignaturasSchema.methods.insert= async function () {
+  //await this.save();
+  await this.save((err, res) => {
+    err ? console.log(err) : "";
+    console.log("saved: " + res);
+  });
+};
+
+AsignaturasSchema.methods.update= async (id, asignatura) => {
+  const Asignatura = mongoose.model("asignaturas", AsignaturasSchema);
+  await Asignatura.updateOne({_id: id}, asignatura, err => {
+    if (err) console.log(err);
+  });
+  console.log(id + " updated");
+};
+
+AsignaturasSchema.methods.delete= async function (id) {
+  const Asignatura = mongoose.model("asignaturas", AsignaturasSchema);
+  await Asignatura.deleteOne({_id: id}, err => {
+    if (err) console.log(err);
+  });
+  console.log(id + " deleted");
+
+};
+
+AsignaturasSchema.methods.findById= async function (id) {
+  const Asignatura = mongoose.model("asignaturas", AsignaturasSchema);
+  return await Asignatura.findById(id);
+};
+
+AsignaturasSchema.methods.findSearch= async function (search, usuario) {
+  const Asignatura = mongoose.model("asignaturas", AsignaturasSchema);
+  return await Asignatura.find({'title' : new RegExp(search, 'i'),'usuario': usuario});
+};
+
+
+module.exports = mongoose.model('asignaturas', AsignaturasSchema);
